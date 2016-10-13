f1068aac683d72ce00e69fea57fabcdb652ffb54
Tong Wang <twan@loc.gov>
2016-10-13 17:08:36 -0400

    multiple groups. added updating group stats in dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,8 +4,15 @@ class Admin::DashboardController < Admin::AdminBaseController
   end
 
   def recalculate_stats
+    # calculate project's stats
     project = Project.current
     project.check_and_update_stats
+
+    # calculate group's stats
+    Group.all.each do |group|
+      group.check_and_update_stats
+    end
+
     render :json => {:project => project, :stats => project.stats}
   end
 end
